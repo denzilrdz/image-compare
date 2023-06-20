8f06cdc72d1e365a7d18310ee9ba64ef3d680208
Denzil Rodrigues <denzil.rdz@gmail.com>
2023-06-20 17:53:32 +0530

    Added initial tests

---
+++ src/tests/test_verify.py
@@ -0,0 +1,31 @@
+import unittest
+from src.image_compare import verify_images
+
+
+class TestVerifyImages(unittest.TestCase):
+    def test_incorrect_file_path(self):
+        self.assertRaises(FileNotFoundError, verify_images, "test_1.png", "test_2.png")
+
+    def test_same_images(self):
+        res = verify_images(
+            "src/tests/test_img/test_1.png", "src/tests/test_img/test_1.png"
+        )
+        self.assertTrue(res)
+
+    def test_similar_images_0_0001_threshold(self):
+        res = verify_images(
+            "src/tests/test_img/test_1.png", "src/tests/test_img/test_2.png"
+        )
+        self.assertFalse(res)
+
+    def test_similar_images_0_1_threshold(self):
+        res = verify_images(
+            "src/tests/test_img/test_1.png", "src/tests/test_img/test_2.png", 0.1
+        )
+        self.assertTrue(res)
+
+    def test_different_images(self):
+        res = verify_images(
+            "src/tests/test_img/test_1.png", "src/tests/test_img/test_3.png"
+        )
+        self.assertFalse(res)
